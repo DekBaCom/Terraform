fb745868a89057b6771d2f4340dc231a3a9c86ca
DekBaCom <abdulloh.eg@gmail.com>
2023-11-10 14:28:05 +0700

    add save stage in cloud on main.tf

--- Completed-Code/Azure/main.tf
+++ Completed-Code/Azure/main.tf
@@ -13,7 +13,12 @@ terraform {
       version = "3.79.0"
     }
   }
-  xxxxx
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "<storage_account_name>"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 
